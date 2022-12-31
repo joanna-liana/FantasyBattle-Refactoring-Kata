488eac45625c29df2bdf9e9ff817ac234abd0238
Joanna Liana <joanna_liana@outlook.com>
2022-12-31 16:52:54 +0100

    Remove redundant getters from equipment

--- go/equipment.go
+++ go/equipment.go
@@ -14,46 +14,18 @@ func MakeEquipment(leftHand Item, rightHand Item, head Item, feet Item, chest It
 	return Equipment{leftHand, rightHand, head, feet, chest}
 }
 
-func (e Equipment) GetLeftHand() Item {
-	return e.leftHand
-}
-func (e Equipment) GetRightHand() Item {
-	return e.rightHand
-}
-func (e Equipment) GetHead() Item {
-	return e.head
-}
-func (e Equipment) GetFeet() Item {
-	return e.feet
-}
-func (e Equipment) GetChest() Item {
-	return e.chest
-}
-
 func (e Equipment) GetBaseDamage() int32 {
-	leftHand := e.GetLeftHand()
-	rightHand := e.GetRightHand()
-	head := e.GetHead()
-	feet := e.GetFeet()
-	chest := e.GetChest()
-
-	return leftHand.GetBaseDamage() +
-		rightHand.GetBaseDamage() +
-		head.GetBaseDamage() +
-		feet.GetBaseDamage() +
-		chest.GetBaseDamage()
+	return e.leftHand.GetBaseDamage() +
+		e.rightHand.GetBaseDamage() +
+		e.head.GetBaseDamage() +
+		e.feet.GetBaseDamage() +
+		e.chest.GetBaseDamage()
 }
 
 func (e Equipment) GetDamageModifier() float64 {
-	leftHand := e.GetLeftHand()
-	rightHand := e.GetRightHand()
-	head := e.GetHead()
-	feet := e.GetFeet()
-	chest := e.GetChest()
-
-	return leftHand.GetDamageModifier() +
-		rightHand.GetDamageModifier() +
-		head.GetDamageModifier() +
-		feet.GetDamageModifier() +
-		chest.GetDamageModifier()
+	return e.leftHand.GetDamageModifier() +
+		e.rightHand.GetDamageModifier() +
+		e.head.GetDamageModifier() +
+		e.feet.GetDamageModifier() +
+		e.chest.GetDamageModifier()
 }
